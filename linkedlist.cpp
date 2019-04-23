644e54205decd9645f608b28728fb266dccf869d
sahilsheth <32227270+sahilsheth@users.noreply.github.com>
2019-04-23 10:17:54 -0700

    Add files via upload

---
+++ linkedlist.cpp
@@ -0,0 +1,67 @@
+#include "linkedlist.h"
+
+linkedlist::linkedlist() {
+   this->data = 0;
+   this->next = NULL;
+}
+
+linkedlist::~linkedlist() {
+    // RECURSIVE
+    Node* curr = next;
+    Node* anotherNode = NULL;
+    while (curr != NULL) {
+        anotherNode = curr;
+        curr = curr->curr;
+        delete anotherNode;
+    }
+
+    or delete next;
+}
+
+int linkedlist::count() {
+    // RECURSIVE
+   Node *title = next;
+   if(title == NULL)
+   {
+      return 0;
+   }
+   return 1+count(title->next);
+    
+}
+
+void linkedlist::addToFront(int n) {
+
+}
+
+double linkedlist::average() {
+    // uses sum and count functions
+}
+
+int linkedlist::sum() {
+    // RECURSIVE
+    if(next == NULL)
+    {
+      return 0;
+    }
+    else
+    {
+       return next->data + Sum(next->next);
+    }
+
+  /*
+  int Sum(Node *head)
+ {
+    if ( head != NULL )
+        return head->data + Sum(head->next);
+    else
+        return 0;
+ }*/
+}
+
+void linkedlist::writeInorder(string & file) {
+
+}
+
+void linkedlist::writeReversed(string & file) {
+    // RECURSIVE
+}
