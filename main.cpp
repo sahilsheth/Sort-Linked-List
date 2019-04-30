2f3cfa5fd146dc8eea19761b762d1bbb2865af20
sahilsheth <32227270+sahilsheth@users.noreply.github.com>
2019-04-30 10:45:21 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,26 @@
 using namespace std;
 
 void readFile(linkedlist & list, string & file) {
-
+    ifstream readIn;
+    int tempNum = 0;
+    readIn.open(file);
+    while(readIn >> tempNum)
+    {
+        list.addToFront(tempNum);
+    }
+    readIn.close();
 }
 
 int main() {
 
+    linkedlist myNumList = linkedlist();
+    string enterFile = "nums.txt";
+    readFile(myNum, enterFile);
+    cout << "Average is: " << myNumList.average() << endl;
+    myNumList.writeInorder(enterFile);
+    myNumList.writeReversed(enterFile);
+    
+    delete myNum;
+    
     return 0;
 }
